76aac0b66c5be37a53960e185f4885e8e723f8e6
Daniel Mera de Agustin <daniel.meradeagustin@gmail.com>
2018-11-21 15:46:20 +0100

    working as per first requirements

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+counter = 1
+category = Restaurant::RESTAURANT_CATEGORIES
+5.times do
+  rest = Restaurant.new
+  rest.name = "restaurant-#{counter}"
+  rest.address = "address-#{counter}"
+  rest.phone_number = "phone-#{counter}"
+  rest.category = category.sample
+  rest.save
+  counter += 1
+end
